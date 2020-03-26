aef39c8d352f9dfebf7c965c770a552e7e46602c
elDin-Dev <alexmaildese-dev@yahoo.com>
2020-03-27 00:45:28 +0100

    unsave

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -13,7 +13,7 @@ class FullPost extends Component {
 
         if (this.props.id) {
 
-            if ( (!this.state.loadedPost) || (this.state.loadedPost.id !== this.props.id)) {
+            if ( (!this.state.loadedPost) || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
                 axios.get("https://jsonplaceholder.typicode.com/posts/" + this.props.id)
                     .then(response => {
                         console.log(response);
