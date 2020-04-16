e7d67dc1b990215585e1ab122a6e3bcd3a4f993b
elDin-Dev <alexmaildese-dev@yahoo.com>
2020-04-17 01:05:20 +0200

    roting to a component

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -6,7 +6,7 @@ import "./Blog.css";
 
 class Blog extends Component {
 
-  render() {
+  render() { 
 
     return (
       <div className="Blog">
@@ -20,8 +20,12 @@ class Blog extends Component {
             </ul>
           </nav>
         </header>
+         {/*
           <Route path="/" exact render={ () => <h1>Home</h1>  }/>
           <Route path="/"  render={ () => <h1>Home 2</h1>  }/>
+         */}
+
+        <Route path="/"  exact component ={Posts}  />
       </div>
     );
   }
