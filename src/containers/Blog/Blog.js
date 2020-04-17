eae70142a446d749106a5b85b0c1e00a2195b2b4
elDin-Dev <alexmaildese-dev@yahoo.com>
2020-04-17 19:01:14 +0200

    using navLink with router

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Posts from './Posts/Posts';
-import { Route, Link } from 'react-router-dom'
+import { Route, NavLink } from 'react-router-dom'
 import "./Blog.css";
 import NewPost from './NewPost/NewPost';
 
@@ -15,12 +15,20 @@ class Blog extends Component {
           <nav>
             <ul>
               <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to={{
+                <li><NavLink
+                   to="/"
+                   exact
+                   activeClassName="my-active"
+                   activeStyle={{
+                     color:'#fa923f',
+                     textDecoration:'underline',
+                   }}
+                   >Home</NavLink></li>
+                <li><NavLink to={{
                   pathname: '/new-post',
                   hash: '#submit',
                   search: '?quick-submit=true'
-                }}>New Post</Link></li>
+                }} >New Post</NavLink></li>
               </ul>
             </ul>
           </nav>
